5d6c94e04cab38802d6bf56c64760523f69af58c
DavidingPlus <davidingplus@qq.com>
2025-09-13 22:35:31 +0800

    feat: try using vite build instead

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+
+export default defineConfig({
+  plugins: [vue(), dts({ outDir: 'dist/types' }), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'VitepressThemeWebsite',
+      fileName: format => `vitepress-theme-website.${format}.js`,
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['vue', 'vitepress'],
+      output: { globals: { vue: 'Vue', vitepress: 'VitePress' } }
+    },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
